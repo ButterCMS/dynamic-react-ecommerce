036978f6c16ceb1151f4c1fe9875eb3a4471187f
Chineta Adinnu <52791481+Netacci@users.noreply.github.com>
2022-05-13 09:56:09 +0100

    ecommerce app with buttercms

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from '@iconify/react';
+
+const ComingSoon = () => {
+  return (
+    <div className=' md:p-20 p-7 text-center'>
+      <div className=' flex flex-col md:flex-row place-items-center justify-center text-3xl font-bold'>
+        This page is not ready yet
+        <Icon
+          className='text-rose-800 ml-3 mr-3 hidden md:block'
+          icon='el:arrow-right'
+        />
+        <Icon
+          className='text-rose-800 ml-3 mr-3 mt-4 mb-4 text-4xl md:hidden'
+          icon='entypo:arrow-down'
+        />
+        <Link
+          to='/products'
+          className=' text-white bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300 font-medium transition-all ease-in duration-300  rounded-lg text-sm px-5 py-2.5 text-center'
+        >
+          Go to Products
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
